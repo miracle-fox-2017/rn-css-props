6b24d4b88a0a2ee5c659eab22fb32ddde7c97c0c
Rizky <co.terbang@gmail.com>
2017-12-22 11:00:58 +0700

    third commit

--- livecode/GameScreen.js
+++ livecode/GameScreen.js
@@ -22,43 +22,45 @@ class GameScreen extends Component<{}> {
   
   constructor(props) {
     super()
+    let currentWord = props.words[Math.floor(Math.random() * props.words.length)]
     this.state = {
-      words: props.heroes,
-      // currentPlayer: 'x'
+      word: currentWord,
+      letters: ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
+      guessedWord: '',
+      usedLetter: []
     }
   }
   
+  
   render() {
-    {console.log(this.state.cells)}
-    debugger
     return (
       <View style={styles.container}>
-        <Text>{this.state.words[0]}</Text>
-        <View style= {styles.board}>
-            <TouchableOpacity onPress={() => this.changeColumn(0) }><Text style ={styles.cell}>{this.state.cells[0]}</Text></TouchableOpacity>
-            <TouchableOpacity onPress={() => this.changeColumn(1) }><Text style ={styles.cell}>{this.state.cells[1]}</Text></TouchableOpacity>
-            <TouchableOpacity onPress={() => this.changeColumn(2) }><Text style ={styles.cell}>{this.state.cells[2]}</Text></TouchableOpacity>
-            <TouchableOpacity onPress={() => this.changeColumn(3) }><Text style ={styles.cell}>{this.state.cells[3]}</Text></TouchableOpacity>
-            <TouchableOpacity onPress={() => this.changeColumn(4) }><Text style ={styles.cell}>{this.state.cells[4]}</Text></TouchableOpacity>
-            <TouchableOpacity onPress={() => this.changeColumn(5) }><Text style ={styles.cell}>{this.state.cells[5]}</Text></TouchableOpacity>
-            <TouchableOpacity onPress={() => this.changeColumn(6) }><Text style ={styles.cell}>{this.state.cells[6]}</Text></TouchableOpacity>
-            <TouchableOpacity onPress={() => this.changeColumn(7) }><Text style ={styles.cell}>{this.state.cells[7]}</Text></TouchableOpacity>
-            <TouchableOpacity onPress={() => this.changeColumn(8) }><Text style ={styles.cell}>{this.state.cells[8]}</Text></TouchableOpacity>
+        <Text style={styles.question}>{this.state.word}</Text>
+        <View style={styles.board}>
+          {this.state.letters.map((letter, i) => {
+            if (this.state.usedLetter.indexOf(letter) === -1)
+            return (
+              <TouchableOpacity key={i} onPress={() => this.check(letter)}><Text style ={styles.cell}>{letter}</Text></TouchableOpacity>
+            )
+            else {
+              return (
+                <TouchableOpacity key={i}><Text style ={styles.usedCell}>{letter}</Text></TouchableOpacity>
+              )
+            }
+          })}
         </View>
       </View>
     )
   }
   
-  // changeColumn (e) {
-  //   let temp = this.state.cells
-  //   temp[e] = this.state.currentPlayer
-  //   let nextPlayer = ''
-  //   this.state.currentPlayer === 'o' ? nextPlayer = 'x' : nextPlayer = 'o'
-  //   this.setState({
-  //     cells: temp,
-  //     currentPlayer: nextPlayer
-  //   })
-  // }
+  check (letter) {
+    if (this.state.word.indexOf(letter) > -1) {
+      console.log("benar")
+    }
+    else {
+      console.log("salah")
+    }
+  }
 }
 
 const styles = StyleSheet.create({
@@ -86,19 +88,37 @@ const styles = StyleSheet.create({
     // alignItems: 'center'
   },
   cell: {
-    width: (Dimensions.get('window').width/4),
-    height: (Dimensions.get('window').height/6),
+    width: (Dimensions.get('window').width/8),
+    height: (Dimensions.get('window').height/25),
     backgroundColor: 'skyblue',
-    marginHorizontal: Dimensions.get('window').width/120,
+    marginHorizontal: Dimensions.get('window').width/220,
     marginVertical: Dimensions.get('window').height/20,
-    fontSize: 80
-    // margin: Dimensions.get('window').width/20 Dimensions.get('window').height/20
-    // marginLeft : (Dimensions.get('window').width/20),
-    // marginBottom: (Dimensions.get('window').height/20)
+    fontSize: 20
+  },
+  question: {
+    fontSize: 50
+  },
+  usedCell: {
+    width: (Dimensions.get('window').width/8),
+    height: (Dimensions.get('window').height/25),
+    backgroundColor: 'red',
+    marginHorizontal: Dimensions.get('window').width/220,
+    marginVertical: Dimensions.get('window').height/20,
+    fontSize: 20
   }
 });
 
-
+// <View style= {styles.board}>
+//     <TouchableOpacity onPress={() => this.changeColumn(0) }><Text style ={styles.cell}>{this.state.cells[0]}</Text></TouchableOpacity>
+//     <TouchableOpacity onPress={() => this.changeColumn(1) }><Text style ={styles.cell}>{this.state.cells[1]}</Text></TouchableOpacity>
+//     <TouchableOpacity onPress={() => this.changeColumn(2) }><Text style ={styles.cell}>{this.state.cells[2]}</Text></TouchableOpacity>
+//     <TouchableOpacity onPress={() => this.changeColumn(3) }><Text style ={styles.cell}>{this.state.cells[3]}</Text></TouchableOpacity>
+//     <TouchableOpacity onPress={() => this.changeColumn(4) }><Text style ={styles.cell}>{this.state.cells[4]}</Text></TouchableOpacity>
+//     <TouchableOpacity onPress={() => this.changeColumn(5) }><Text style ={styles.cell}>{this.state.cells[5]}</Text></TouchableOpacity>
+//     <TouchableOpacity onPress={() => this.changeColumn(6) }><Text style ={styles.cell}>{this.state.cells[6]}</Text></TouchableOpacity>
+//     <TouchableOpacity onPress={() => this.changeColumn(7) }><Text style ={styles.cell}>{this.state.cells[7]}</Text></TouchableOpacity>
+//     <TouchableOpacity onPress={() => this.changeColumn(8) }><Text style ={styles.cell}>{this.state.cells[8]}</Text></TouchableOpacity>
+// </View>
 
 const mapStateToProps = state => {
   return {
